93dacfb36ec4abb0d50bcc867945191f2889940c
gabrielts212 <gabrielts2122@gmail.com>
2023-02-27 03:26:46 -0300

    teste

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Box,Flex} from '@chakra-ui/react'
+const Header = () => {
+  return (
+    <div>
+<Flex>
+aaaaaaaaaaa
+</Flex>
+
+    </div>
+  )
+}
+
+export default Header
